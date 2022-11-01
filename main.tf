7d83fe07d3b914518a743d6fc0c1a7a35f263cd3
gogineniprathyusha <104327062+gogineniprathyusha@users.noreply.github.com>
2022-11-01 09:45:31 -0400

    democommit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+ required_providers {
+     aws = {
+         source = "hashicorp/aws"
+         version = "~>4.0"
+     }
+ }
+}
+
+# Configure the AWS provider
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+
+resource "aws_instance" "jenkinspipeline_terraform" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name = "Devops"
+  subnet_id = aws_subnet.Tomcat-Subnet1.id
+  associate_public_ip_address = true
+  user_data = file("softwareinstall.sh")
+  tags = {
+    Name = "jenkinspipeline_terraform"
+}
+}
